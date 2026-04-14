a030dc873b5562b1d540e97380cfefa1f96edbfb
zhennnn215 <doreen950215@gmail.com>
2026-04-14 23:12:31 +0800

    Add files via upload

---
+++ app.py
@@ -0,0 +1,23 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/test')
+def test():
+    return render_template('test.html')
+
+@app.route('/work')
+def work():
+    return render_template('work.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
